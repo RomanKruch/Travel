6168d5778f7f64a40a9497ce05e743139b13164b
ItSites <front3@itsites.com.ua>
2025-01-06 18:12:07 +0200

    add new LOGO

--- src/Modules/Header/Header.tsx
+++ src/Modules/Header/Header.tsx
@@ -2,22 +2,23 @@ import s from './Header.module.css';
 import Logo from '../../icons/Logo';
 import Navigation from '../../components/Navigation/Navigation';
 import ThemeToggle from '../../components/ThemeToggle/ThemeToggle';
+import { NavLink } from 'react-router-dom';
 
 interface IProps {
-    isToggled: boolean;
-    setIsToggled:  React.Dispatch<React.SetStateAction<boolean>>;
+  isToggled: boolean;
+  setIsToggled: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Header = ({isToggled, setIsToggled}: IProps) => {
+const Header = ({ isToggled, setIsToggled }: IProps) => {
   return (
     <header className={s.header}>
-      <a href="/" className={s.logo}>
-        travel
-      </a>
+      <NavLink to="/" className={s.logo}>
+        <Logo />
+      </NavLink>
 
       <Navigation />
 
-      <ThemeToggle isToggled={isToggled} setIsToggled={setIsToggled}/>
+      <ThemeToggle isToggled={isToggled} setIsToggled={setIsToggled} />
     </header>
   );
 };
