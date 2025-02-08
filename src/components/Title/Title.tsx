9ed1570378c876fc345734f322fc6fecbcd7d8b0
ItSites <front3@itsites.com.ua>
2025-02-08 19:48:26 +0200

    adding adaptive on login modals

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,10 +3,11 @@ import s from './Title.module.css';
 
 interface IProps {
   children?: ReactElement | string;
+  className?: string;
 }
 
-const Title = ({ children }: IProps) => {
-  return <h2 className={s.title}>{children}</h2>;
+const Title = ({ children, className = '' }: IProps) => {
+  return <h2 className={`${s.title} ${className}`}>{children}</h2>;
 };
 
 export default Title;
