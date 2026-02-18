38234f86eada16f8ce05747ae5233fd860012140
ItSites <front3@itsites.com.ua>
2026-02-18 11:23:30 +0200

    changed footer text

--- src/Modules/Footer/Footer.tsx
+++ src/Modules/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         TRAVEL
       </NavLink>
 
-      <p className={s.copyright}>© 2025 by Roman Kruch & GPT-chat :)</p>
+      <p className={s.copyright}>© 2025 by Krychkivskyi Roman</p>
       <p className={s.copyright}>
         Github:{' '}
         <a className={s.link} href="https://github.com/RomanKruch" target="_blank">
